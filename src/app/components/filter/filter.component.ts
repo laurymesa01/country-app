188beb9052d8ea06faaf14ccce0fd0a37bcc97d9
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-10-08 11:52:48 -0500

    Creating a filter to filter countries by region

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Region } from '../../models/country.model';
+import { KeyValuePipe } from "@angular/common";
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [KeyValuePipe],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
+
+  regions = Region;
+  isFilterByRegionsMenuOpen: boolean = false;
+
+  openFilter(){
+    this.isFilterByRegionsMenuOpen = !this.isFilterByRegionsMenuOpen;
+  }
+
+
+
 
 }
--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../../components/header/header.component";
 import { SearchComponent } from "../../components/search/search.component";
+import { FilterComponent } from "../../components/filter/filter.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, SearchComponent],
+  imports: [FilterComponent, HeaderComponent, RouterOutlet, SearchComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
