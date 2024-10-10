bbde44924bf0e57094e677087736ac3eb6994c15
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-10-10 15:45:02 -0500

    Switching between dark and light mode

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  @HostBinding('class.dark') get mode() {return this.darkMode(); }
+
+  darkMode = signal<boolean>(false);
+  checked: boolean = false;
+  initialize:boolean = false;
+
+
+  constructor(){
+    if (typeof window !== "undefined") {
+      this.darkMode.set(JSON.parse(window.localStorage.getItem("darkMode") ?? "false"))
+      if (this.darkMode() === true) {
+        this.checked = true;
+      }
+      this.initialize = true;
+      effect(() => {
+        window.localStorage.setItem('darkMode', JSON.stringify(this.darkMode()));
+      });
+    }
+  }
+
+  changeMode(){
+    this.darkMode.set(!this.darkMode());
+  }
+
 
 }
